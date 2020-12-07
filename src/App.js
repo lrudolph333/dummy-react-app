090bafb535ab413756a873bfc5ab4f503b37ea05
lrudolph333 <trey.ru.iii@gmail.com>
2020-12-07 15:01:48 -0800

    html and css transferred

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+      <div class="header">
+          <h1>
+              My To-Do list
+          </h1>
+      </div>
+      <div id="addNewTodoContainer">
+          <form>
+              <h5>
+                  Add New To-Do
+              </h5>
+              <div class="form-group">
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary" id="addButton">Add</button>
+          </form>
+      </div>
+      <div id="todoListContainer">
+          <h5>
+              Current To-Do List
+          </h5>
+          <ul class="list-group">
+
+          </ul>
+      </div>
+  </div>
   );
 }
 
